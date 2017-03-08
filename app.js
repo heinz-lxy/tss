980b58ce7b023353d5e51ad4e12517a07eca8afb
Heinz <heinz.lxy@gmail.com>
2017-03-09 00:44:01 +0800

    done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let playGround = new Array(100) //棋盘
 let realPlayGround = $('#playGround')
 let path=[{x:7,y:7}]	//蛇的起始位置
+let directionSet=[{i:0,j:-1},{i:1,j:0},{i:0,j:1},{i:-1,j:0}]
+
+
 
 // let app  = {
 // 	init:function{
@@ -9,24 +12,79 @@ let path=[{x:7,y:7}]	//蛇的起始位置
 // }
 
 function coor2Index(coor){
-	var {x,y} = coor
-	var index = (y-1)*10+x
+	let {x,y} = coor
+	let index = (y-1)*10+x
 	return index
 }
 
 
 function render(coor,object){
 	let index = coor2Index(coor)
-	realPlayGround.find('td').eq(index-1).append(object)
+	realPlayGround.find('td').eq(index-1).html(object)
+}
+
+function clear(coor){
+	let index = coor2Index(coor)
+	realPlayGround.find('td').eq(index-1).empty()
+}
+
+function judge(coor){
+	if(coor.x===0||coor.x===11||coor.y===0||coor.y===11){ //越界
+		return 1 //game over
+	}else if(arrayContainsObj(path,coor)){ //碰到自身轨迹
+		console.log('hha')
+		return 1
+	}else{
+		return 0
+	}
+}
+// 判断对象值是否相等
+function isEqual(obj1,obj2){
+	for(prop in obj1){
+		if(obj1[prop]!==obj2[prop])
+			return false
+	}
+	return true
+}
+
+function arrayContainsObj(array,obj){
+	for(var i=0;i<array.length;i++){
+		if(isEqual(array[i],obj)){
+			return true
+		}
+	}
+	return false
 }
 
 
-realPlayGround.on('keydown',function(e){
-	let key=e.which
-	// switch(key):
-	// 	case 37:
+function events(){
+	$('body').on('keydown',function(e){
+		let key=e.which
+		let direction
+		switch(key){
+			case 37:
+				direction = 3 //left
+				break
+			case 38:
+				direction = 0 //up
+				break
+			case 39:
+				direction = 1 //right
+				break
+			case 40:
+				direction = 2 //down
+				break
+		}
+		if(snake1!==undefined){
+		    snake1.changeDirection(direction)
+		}
+	    else{
+	    	return
+	    }  
 
-})
+	})
+}
+
 
 class food{
 	constructor(){
@@ -34,8 +92,13 @@ class food{
 		this.render(this.coor)
 	}
 	newFood(){
+		clear(this.coor)
 		this.coor = {x:Math.ceil(Math.random()*10),y:Math.ceil(Math.random()*10)}
-		this.render(coor)
+		if(path.indexOf(this.coor)!==-1){
+			newFood()
+		}else{
+			this.render(this.coor)
+		}
 	}
 
 	render(coor){
@@ -49,36 +112,55 @@ class food{
 
 class snake{
 	constructor(){
-		let direction = 1 //0 1 2 3 上 右 下 左
+		this.direction = 1 //0 1 2 3 上 右 下 左
 		this.renderPath()
 	}
 
 	move(){
-		var self=this
-		setInterval(function(){
+		let self=this
+		let id = setInterval(function(){
 			let coor = path[path.length-1]
 			// coor++
-			console.log(path)
-			coor = {x:(coor.x+1),y:coor.y}
-			if(coor!=food.coor){
-				path.push(coor)
-				path.shift()
-				self.renderPath()
+			// coor = {x:(coor.x+1),y:coor.y}
+			coor=self.coorPlus(coor)
+			if(judge(coor)){
+				alert('Game Over')
+				clearInterval(id)
+				return
 			}
+			path.push(coor)
+			if(isEqual(coor,food1.coor)){
+				food1.newFood()
+			}else{
+				// 清除最后一格
+				clear(path[0])
+				path.shift()
+			}
+			self.renderPath()
 		},500)
 
 	}
 
 	renderPath(){
-		var snake=2
+		let snake=2
 		for(let i=0;i<path.length;i++){
 			render(path[i],snake)
 		}
 	}
+
+	changeDirection(direction){
+		this.direction=direction
+	}
+
+	coorPlus(coor){
+		let {x,y} = coor
+	    let {i,j}=directionSet[this.direction]
+	    return {x:(x+i),y:(y+j)}
+	}
 }
 
 
-
+events()
 let food1 = new food()
 let snake1 = new snake()
 snake1.move()
